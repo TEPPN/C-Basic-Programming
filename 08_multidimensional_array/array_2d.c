873ba9f7870f20f3c97c3626d356c3860646e09a
aqilwayne <139626119+aqilwayne@users.noreply.github.com>
2025-10-26 22:53:06 +0700

    Update array_2d.c

--- 08_multidimensional_array/array_2d.c
+++ 08_multidimensional_array/array_2d.c
@@ -3,7 +3,7 @@ int main() {
     // Create and initialize an array with 3 rows and 2 columns
     int arr[3][2] = { { 0, 1 }, { 2, 3 }, { 4, 5 } };
 
-    // Print each array element's value
+    // Print each array element's value with looping
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 2; j++) {
             printf("arr[%d][%d]: %d    ", i, j, arr[i][j]);
@@ -12,3 +12,4 @@ int main() {
     }
     return 0;
 }
+
